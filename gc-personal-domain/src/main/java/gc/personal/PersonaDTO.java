a35b307e0b10f9e0c3b21f553051d9895df7546a
Pilar <mrodlav@gobiernodecanarias.org>
2019-11-28 15:01:14 +0100

    Add initial project

---
+++ gc-personal-domain/src/main/java/gc/personal/PersonaDTO.java
@@ -0,0 +1,12 @@
+
+package gc.personal;
+
+public class PersonaDTO {
+   private Integer id; 
+   private String name;
+   
+   public PersonaDTO(){ 
+      this.id = null;
+      this.name = null;
+   }
+}
